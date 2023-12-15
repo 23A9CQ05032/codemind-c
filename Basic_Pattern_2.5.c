b111f3142bd6484f49cb4f4eca250786c19b6874
N. Tejasree <147476028+Tejasree2308@users.noreply.github.com>
2023-12-15 21:47:45 +0530

    #include<stdio.h> int main() {     int i,j,n,k=1;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=n;j>=i;j--)         {             printf("%c ",i+64);         }         printf(" ");         k++;     } }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,k=1;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=n;j>=i;j--)
+        {
+            printf("%c ",i+64);
+        }
+        printf("
+");
+        k++;
+    }
+}
