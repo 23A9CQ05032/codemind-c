13185cf045e2e559f1e5af17b50ba4c83707710d
N. Tejasree <147476028+Tejasree2308@users.noreply.github.com>
2023-12-20 10:12:29 +0530

    #include<stdio.h> int main() {     int a,i,sum=0,avg;     scanf("%d",&a);     int arr[a], flag =0;     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<a;i++)     {         sum=sum+arr[i];     }     avg=sum/a;     for(i=0;i<a;i++)     {         if(avg<=arr[i])         {             flag++;         }     }     printf("%d",flag);
    }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,sum=0,avg;
+    scanf("%d",&a);
+    int arr[a], flag =0;
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<a;i++)
+    {
+        sum=sum+arr[i];
+    }
+    avg=sum/a;
+    for(i=0;i<a;i++)
+    {
+        if(avg<=arr[i])
+        {
+            flag++;
+        }
+    }
+    printf("%d",flag);
+        
+}
