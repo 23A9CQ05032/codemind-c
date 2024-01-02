04477328b336d1b70e82ac184efbfd370f954ff4
N. Tejasree <147476028+Tejasree2308@users.noreply.github.com>
2024-01-02 18:25:50 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     double arr[100];     for(i=0;i<n;i++)     {         scanf("%lf",&arr[i]);     }     int a,b,c=0;     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]<a||arr[i]>b)         {             printf("%0.lf ",arr[i]);             c++;         }     }     if (c==0) printf("-1"); }

---
+++ Elements_not_in_between_A_and_B.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    double arr[100];
+    for(i=0;i<n;i++)
+    {
+        scanf("%lf",&arr[i]);
+    }
+    int a,b,c=0;
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<a||arr[i]>b)
+        {
+            printf("%0.lf ",arr[i]);
+            c++;
+        }
+    }
+    if (c==0) printf("-1");
+}
